172599b17cc7b5ecfcc419c6bdee76d1008c6164
pd9333 <pd9333@gmail.com>
2023-04-02 12:55:23 +0800

    refactor: remove redundant checks
    For example, we don't need to check both (degree >= 22.5 && degree < 45) because failing the previous one `if (degree <
    22.5)` already means degree is >= 22.5

--- wind-direction-from-degree.ts
+++ wind-direction-from-degree.ts
@@ -2,19 +2,19 @@ export function windDirectionFromDegree(degree) {
   degree = (degree + 11.25) % 360;
 
   if (degree < 22.5) return "N";
-  else if (degree >= 22.5 && degree < 45) return "NNE";
-  else if (degree >= 45 && degree < 67.5) return "NE";
-  else if (degree >= 67.5 && degree < 90) return "ENE";
-  else if (degree >= 90 && degree < 112.5) return "E";
-  else if (degree >= 112.5 && degree < 135) return "ESE";
-  else if (degree >= 135 && degree < 157.5) return "SE";
-  else if (degree >= 157.5 && degree < 180) return "SSE";
-  else if (degree >= 180 && degree < 202.5) return "S";
-  else if (degree >= 202.5 && degree < 225) return "SSW";
-  else if (degree >= 225 && degree < 247.5) return "SW";
-  else if (degree >= 247.5 && degree < 270) return "WSW";
-  else if (degree >= 270 && degree < 292.5) return "W";
-  else if (degree >= 292.5 && degree < 315) return "WNW";
-  else if (degree >= 315 && degree < 337.5) return "NW";
-  else if (degree >= 337.5) return "NNW";
+  if (degree < 45) return "NNE";
+  if (degree < 67.5) return "NE";
+  if (degree < 90) return "ENE";
+  if (degree < 112.5) return "E";
+  if (degree < 135) return "ESE";
+  if (degree < 157.5) return "SE";
+  if (degree < 180) return "SSE";
+  if (degree < 202.5) return "S";
+  if (degree < 225) return "SSW";
+  if (degree < 247.5) return "SW";
+  if (degree < 270) return "WSW";
+  if (degree < 292.5) return "W";
+  if (degree < 315) return "WNW";
+  if (degree < 337.5) return "NW";
+  if (degree < 360) return "NNW";
 }
