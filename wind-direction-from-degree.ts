571c2d3eeaf807d6ce36b4524471592cf57e667c
pd9333 <pd9333@gmail.com>
2023-04-02 10:44:01 +0800

    feat: original code

---
+++ wind-direction-from-degree.ts
@@ -0,0 +1,16 @@
+if (wind_deg >= 348.75 && wind_deg < 11.25) wind_direction.innerText = "N"
+else if (wind_deg >= 11.25 && wind_deg < 33.75) wind_direction.innerText = "NNE";
+else if (wind_deg >= 33.75 && wind_deg < 56.25) wind_direction.innerText = "NE";
+else if (wind_deg >= 56.25 && wind_deg < 78.75) wind_direction.innerText = "ENE";
+else if (wind_deg >= 78.75 && wind_deg < 101.25) wind_direction.innerText = "E";
+else if (wind_deg >= 101.25 && wind_deg < 123.75) wind_direction.innerText = "ESE";
+else if (wind_deg >= 123.75 && wind_deg < 146.25) wind_direction.innerText = "SE";
+else if (wind_deg >= 146.25 && wind_deg < 168.75) wind_direction.innerText = "SSE";
+else if (wind_deg >= 168.75 && wind_deg < 191.25) wind_direction.innerText = "S";
+else if (wind_deg >= 191.25 && wind_deg < 213.75) wind_direction.innerText = "SSW";
+else if (wind_deg >= 213.75 && wind_deg < 236.25) wind_direction.innerText = "SW";
+else if (wind_deg >= 236.25 && wind_deg < 258.75) wind_direction.innerText = "WSW";
+else if (wind_deg >= 258.75 && wind_deg < 281.25) wind_direction.innerText = "W";
+else if (wind_deg >= 281.25 && wind_deg < 303.75) wind_direction.innerText = "WNW";
+else if (wind_deg >= 303.75 && wind_deg < 326.25) wind_direction.innerText = "NW";
+else if (wind_deg >= 326.25 && wind_deg < 348.75) wind_direction.innerText = "NNW";
