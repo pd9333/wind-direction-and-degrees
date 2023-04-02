4bdf36cbe004086e9b22bb36a2fb30e06cd8b033
pd9333 <pd9333@gmail.com>
2023-04-02 13:36:06 +0800

    refactor: we can calculate the index from degree directly
    So there is no need to perform any kind of search.

--- wind-direction-from-degree.ts
+++ wind-direction-from-degree.ts
@@ -1,34 +1,13 @@
 export function windDirectionFromDegree(degree) {
   degree = (degree + 11.25) % 360;
 
-  const endingDegrees = [
-    22.5, 45, 67.5, 90,
-    112.5, 135, 157.5, 180,
-    202.5, 225, 247.5, 270,
-    292.5, 315, 337.5, 360,
-  ];
   const windDirections = [
     "N", "NNE", "NE", "ENE",
     "E", "ESE", "SE", "SSE",
     "S", "SSW", "SW", "WSW",
     "W", "WNW", "NW", "NNW",
   ];
-  const i = binarySearchFindHeadOfSecondHalf(endingDegrees.length, i => {
-    return degree < endingDegrees[i];
-  });
-  return windDirections[i];
-}
 
-function binarySearchFindHeadOfSecondHalf(size, isSecondHalf) {
-  let begin = 0;
-  let end = size;
-  while (begin < end) {
-    let mid = Math.floor((begin + end) / 2);
-    if (isSecondHalf(mid)) {
-      end = mid;
-    } else {
-      begin = mid + 1;
-    }
-  }
-  return begin;
+  const i = Math.floor(degree / 22.5);
+  return windDirections[i];
 }
